916e66bd48d9d9815d7926f7a82c22d36e2e66f4
Rainer Simon <aboutgeo@gmail.com>
2022-02-09 16:28:20 +0100

    Build setup

--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
-import App from './App.svelte'
+/*import App from './App.svelte'
 
 const app = new App({
   target: document.getElementById('app')
 })
+*/
 
-export default app
+export default {
+
+  init: config => console.log('yay', config)
+
+}
--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,16 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()]
+  plugins: [svelte()],
+  server: {
+    open: '/public/index.html'
+  },
+  build: {
+    sourcemap: true,
+    lib: {
+      entry: './src/main.js',
+      name: 'Annotorious',
+      formats: [ 'umd' ]
+    }
+  }
 })
